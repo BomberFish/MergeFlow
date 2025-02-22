7b5ddc85394b7b816cbe3dee98772d148409c07a
BomberFish <bomberfish@bomberfish.ca>
2025-02-22 14:36:00 -0500

    better argument parsing

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ const model = genAI.getGenerativeModel({
 });
 const prompt = "Solve any git merge conflicts in the file below. Only respond with the final file contents, but do not use markdown code blocks.";
 
-const file = process.argv[2];
+const file = process.argv[0].includes("node") ? process.argv[2] : process.argv[1];
+
+if (file == "-h" || file == "--help") {
+	term("Usage: mergeflow index.js <file>\n");
+	term.processExit(0);
+	process.exit(0);
+} if (!file) {
+	term.red("Please provide a file to generate content from.");
+	term.processExit(1);
+	process.exit(1);
+} else if (!fs.existsSync(file)) {
+	term.red("File does not exist.");
+	term.processExit(1);
+	process.exit(1);
+}
 
 const data = {
 	inlineData: {
@@ -32,5 +46,5 @@ term.moveTo(1, cursorLocation.y);
 term(result.response.text());
 
 fs.writeFileSync(file, Buffer.from(result.response.text(), "base64").toString("utf-8"));
-term.green("\nFile saved successfully.\n");
+term.green("\nFile saved successfully.");
 term.processExit(0);
